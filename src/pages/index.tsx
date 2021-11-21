9928d5bfa7411f6d61344fae2d7b00017741c73f
Nirjhor Nath <nirjhor.nath@gmail.com>
2021-11-21 14:05:04 -0500

    favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import { Box } from "@chakra-ui/react";
 import { AnimatePresence, motion } from "framer-motion";
 import React, { useEffect, useState } from "react";
@@ -26,39 +27,68 @@ const Index = () => {
   }, []);
 
   return (
-    <AnimatePresence>
-      {!loading ? (
-        <>
+    <Box>
+      <Head>
+        <title>Nirjhor Nath</title>
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="theme-color" content="#181818" />
+      </Head>
+      <AnimatePresence>
+        {!loading ? (
+          <>
+            <MotionBox
+              key="content"
+              zIndex="1"
+              bgColor="bg.900"
+              initial={{ height: "100vh" }}
+              animate={{ height: "0" }}
+              exit={{ undefined }}
+              transition={{
+                delay: 0.1,
+                ease: [0.7, 0, 0.07, 1],
+                duration: 1.5,
+              }}
+            />
+            <Layout>
+              <Box width={{ base: "90%", sm: "90%", md: "80%" }} margin="auto">
+                <Hero />
+                <Projects />
+                <About />
+                <Contact />
+              </Box>
+            </Layout>
+          </>
+        ) : (
           <MotionBox
-            key="content"
-            zIndex="1"
-            bgColor="bg.900"
-            initial={{ height: "100vh" }}
-            animate={{ height: "0" }}
-            exit={{ undefined }}
-            transition={{ delay: 0.1, ease: [0.7, 0, 0.07, 1], duration: 1.5 }}
-          />
-          <Layout>
-            <Box width={{ base: "90%", sm: "90%", md: "80%" }} margin="auto">
-              <Hero />
-              <Projects />
-              <About />
-              <Contact />
-            </Box>
-          </Layout>
-        </>
-      ) : (
-        <MotionBox
-          key="loading"
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
-          transition={{ opacity: { duration: 0.5 } }}
-        >
-          <Loading />
-        </MotionBox>
-      )}
-    </AnimatePresence>
+            key="loading"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ opacity: { duration: 0.5 } }}
+          >
+            <Loading />
+          </MotionBox>
+        )}
+      </AnimatePresence>
+    </Box>
   );
 };
 
